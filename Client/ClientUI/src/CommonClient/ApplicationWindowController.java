ad53b7e1d28337f74e9985561081c7ff2bfc9f13
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-03-10 17:57:28 +0200

    Updated pages design

---
+++ Client/ClientUI/src/CommonClient/ApplicationWindowController.java
@@ -0,0 +1,20 @@
+package CommonClient;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ApplicationWindowController implements Initializable
+{
+
+    @FXML
+    private BorderPane mainPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
