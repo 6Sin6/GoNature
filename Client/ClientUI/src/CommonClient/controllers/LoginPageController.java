abdb73a43a7db3c70eb44234d58a1cd0060196de
DESKTOP-1AHT74U\USER <Avishay.Bar10@gmail.com>
2024-03-11 19:48:44 +0200

    try to sign in page

--- Client/ClientUI/src/CommonClient/controllers/LoginPageController.java
+++ Client/ClientUI/src/CommonClient/controllers/LoginPageController.java
@@ -1,9 +1,13 @@
 package CommonClient.controllers;
 
-import CommonClient.controllers.ApplicationWindowController;
-import CommonClient.controllers.BaseController;
+import CommonClient.ClientUI;
+import Entities.Message;
+import Entities.OpCodes;
 import Entities.Role;
+import Entities.User;
+import client.ClientCommunicator;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -11,6 +15,8 @@ import javafx.scene.control.Separator;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+
+import javax.naming.CommunicationException;
 
 public class LoginPageController extends BaseController {
     @FXML
@@ -37,9 +43,38 @@ public class LoginPageController extends BaseController {
     @FXML
     private MFXTextField userNameText;
 
+    @FXML
+    private MFXPasswordField passwordText;
 
-    public void onLoginClick() {
-        applicationWindowController.loadDashboardPage(Role.ROLE_SINGLE_VISITOR);
-        applicationWindowController.loadMenu("Eyal123", "Visitor");
+    @FXML
+    private Label ErrorMsg;
+
+
+    private String getUserName() {
+        return userNameText.getText();
+    }
+
+    private String getPassword() {
+        return passwordText.getText();
+    }
+
+    public void onLoginClick() throws CommunicationException {
+        User user = new User(getUserName(), getPassword());
+        Object msg = new Message(OpCodes.OP_SIGN_IN, getUserName(), user);
+
+        ClientUI.client.accept(msg);
+        Message respondMsg = ClientCommunicator.msg;
+
+        OpCodes returnOpCode = respondMsg.getMsgOpcode();
+        if (returnOpCode != OpCodes.OP_SIGN_IN) {
+            throw new CommunicationException("Respond not appropriate from server");
+        }
+        user = (User) respondMsg.getMsgData();
+        if (user.getRole() != Role.ROLE_GUEST) {
+            applicationWindowController.loadDashboardPage(user.getRole());
+            applicationWindowController.loadMenu(user);
+        } else {
+            ErrorMsg.setText("Wrong username or password ! Please try again !");
+        }
     }
 }
