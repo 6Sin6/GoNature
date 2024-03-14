c6b4d52cb7a92a5171ec61dd17420bc96465a554
Eyal Maklada <wezkc21@gmail.com>
2024-03-14 14:36:31 +0200

    1. Fixed Issue - Adir's Rework [WIP]

--- Client/ClientUI/src/EmployeesControllers/RegisterGroupGuideController.java
+++ Client/ClientUI/src/EmployeesControllers/RegisterGroupGuideController.java
@@ -55,42 +55,7 @@ public class RegisterGroupGuideController extends BaseController {
     @FXML
     void OnClickSubmitButton(ActionEvent event)
     {
-        String groupGuideID = txtGroupGuideID.getText();
 
-        if(groupGuideID != null && groupGuideID.isEmpty())
-        {
-            lblErrorMsg.setText("Please enter a group guide ID");
-            return;
-        }
-
-        //if(!CommonUtils.isValidID(groupGuideID))
-        //{
-         //   lblErrorMsg.setText("Invalid group guide ID, must be 9 digits.");
-         //   return;
-        //}
-
-        Message msg = new Message(OpCodes.OP_REGISTER_GROUP_GUIDE, super.applicationWindowController.getUser().getUsername(), groupGuideID);
-        ClientUI.client.accept(msg);
-        Message respondMsg = ClientCommunicator.msg;
-
-        switch(respondMsg.getMsgOpcode())
-        {
-            case OP_VISITOR_ID_DOESNT_EXIST:
-                lblErrorMsg.setText("Visitor ID does not exist!");
-                break;
-            case OP_VISITOR_IS_ALREADY_GROUP_GUIDE:
-                lblErrorMsg.setText("Visitor is already group guide!");
-                break;
-            case OP_UPDATED_VISITOR_TO_GROUP_GUIDE:
-                MessagePopup popup = new MessagePopup("Group Guide Registered Successfully!", Duration.seconds(3), 500, 300, false);
-                popup.show(applicationWindowController.getRoot());
-                applicationWindowController.setCenterPage("/EmployeesUI/SupportRepresentativeDashboardPage.fxml");
-                break;
-            default:
-                lblErrorMsg.setText("Unknown Error occurred, please try again later.");
-                break;
-
-        }
     }
 
 }
