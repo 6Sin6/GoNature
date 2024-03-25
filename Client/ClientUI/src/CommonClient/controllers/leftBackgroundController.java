5df315cb4450e6104994ba681dd288fdf822937f
6Sin6 <alexsegal666@gmail.com>
2024-03-25 22:22:42 +0200

    Design Sign In Page - Alex(25.3.24)

--- Client/ClientUI/src/CommonClient/controllers/leftBackgroundController.java
+++ Client/ClientUI/src/CommonClient/controllers/leftBackgroundController.java
@@ -1,4 +1,17 @@
 package CommonClient.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class leftBackgroundController {
+
+    @FXML
+    private ImageView btnBackNoMenu;
+
+    @FXML
+    void OnClickBackNoMenuButton(MouseEvent event) {
+
+    }
+
 }
