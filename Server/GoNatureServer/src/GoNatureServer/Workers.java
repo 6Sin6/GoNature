ddcb8ee447e74deb9a0e55fd38f35c2149e8eec9
Shay Bistrizky <shaybis.sb@gmail.com>
2024-03-27 18:08:12 +0200

    fix fix fix orders

--- Server/GoNatureServer/src/GoNatureServer/Workers.java
+++ Server/GoNatureServer/src/GoNatureServer/Workers.java
@@ -126,7 +126,7 @@ public class Workers {
                 calendar.set(Calendar.SECOND, 0);
                 calendar.set(Calendar.MILLISECOND, 0);
                 db.ChangeToAbsent();
-                controller.addtolog("Update status for absent ordets  " + calendar.getTime());
+                controller.addtolog("Update status for absent orders  " + calendar.getTime());
             }
             catch (Exception e){
                 controller.addtolog("failed changeToAbsentOrders");
