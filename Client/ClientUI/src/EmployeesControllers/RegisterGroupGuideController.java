37134bdeb46191be926b0bde94669359985e84b9
Shay Bistrizky <shaybis.sb@gmail.com>
2024-03-29 15:47:44 +0300

    fix fix fix fix

--- Client/ClientUI/src/EmployeesControllers/RegisterGroupGuideController.java
+++ Client/ClientUI/src/EmployeesControllers/RegisterGroupGuideController.java
@@ -60,18 +60,20 @@ public class RegisterGroupGuideController extends BaseController {
             confirmationPopup.show(applicationWindowController.getRoot());
             return;
         }
-        if (!(response.getMsgData() instanceof String)) {
+        if (!(response.getMsgData() instanceof String) && response.getMsgData() != null) {
             ConfirmationPopup confirmationPopup = new ConfirmationPopup(CommonUtils.SERVER_ERROR, applicationWindowController, 800, 400, true, "OK", true);
             confirmationPopup.show(applicationWindowController.getRoot());
             return;
         }
 
-        String msgAnswer = (String) response.getMsgData();
-
-        if (msgAnswer == null)
+        if ( response.getMsgData()==null){
             updateMsg("Group guide has successfully registered!", "#008000");
-        else
-            updateMsg(msgAnswer, "#FF0000");
+        }
+        else{
+            updateMsg((String) response.getMsgData(), "#FF0000");
+        }
+
+
     }
 
     private void updateMsg(String msg, String colorCode) {
