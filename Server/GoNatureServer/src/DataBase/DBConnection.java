ec2db491ff3de4871c16f4742ad7b671e9ad1324
Eyal Maklada <wezkc21@gmail.com>
2024-03-29 14:46:27 +0300

    followup

--- Server/GoNatureServer/src/DataBase/DBConnection.java
+++ Server/GoNatureServer/src/DataBase/DBConnection.java
@@ -618,7 +618,7 @@ public class DBConnection {
     public String setExitTimeOfOrder(String orderID) throws Exception {
         try {
             String tableName = this.schemaName + ".orders";
-            String whereClause = "OrderID='" + orderID + "' AND HOUR(ExitedTime) >= HOUR(CURRENT_TIMESTAMP()) AND HOUR(EnteredTime) <= HOUR(CURRENT_TIMESTAMP()) AND YEAR(VisitationDate) = YEAR(CURRENT_DATE) AND MONTH(VisitationDate) = MONTH(CURRENT_DATE) AND DAY(VisitationDate) = DAY(CURRENT_DATE) AND orderStatus IN ('" + OrderStatus.STATUS_FULFILLED.getOrderStatus() + "', '" + OrderStatus.STATUS_SPONTANEOUS_ORDER.getOrderStatus() + "')";
+            String whereClause = "OrderID='" + orderID + "' AND TIMESTAMP(ExitedTime) >= TIMESTAMP(CURRENT_TIMESTAMP()) AND HOUR(EnteredTime) <= HOUR(CURRENT_TIMESTAMP()) AND YEAR(VisitationDate) = YEAR(CURRENT_DATE) AND MONTH(VisitationDate) = MONTH(CURRENT_DATE) AND DAY(VisitationDate) = DAY(CURRENT_DATE) AND orderStatus IN ('" + OrderStatus.STATUS_FULFILLED.getOrderStatus() + "', '" + OrderStatus.STATUS_SPONTANEOUS_ORDER.getOrderStatus() + "')";
             ResultSet results = dbController.selectRecordsFields(tableName, whereClause, "ExitedTime", "VisitationDate");
             if (!results.next()) {
                 return "Order ineligible to be updated.";
