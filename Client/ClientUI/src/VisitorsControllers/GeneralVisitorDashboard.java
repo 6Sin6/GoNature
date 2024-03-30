63f5f9bd6d487afa5e5bd8736a77744d8bf8bda0
ronben17 <ronbendel123456@gmail.com>
2024-03-30 12:55:25 +0300

    java doc client

--- Client/ClientUI/src/VisitorsControllers/GeneralVisitorDashboard.java
+++ Client/ClientUI/src/VisitorsControllers/GeneralVisitorDashboard.java
@@ -3,5 +3,17 @@ package VisitorsControllers;
 import CommonClient.controllers.BaseController;
 
 public abstract class GeneralVisitorDashboard extends BaseController {
+
+    /**
+     * Retrieves the unique identifier for the current user.
+     * <p>
+     * This method is abstract and must be implemented by subclasses to provide
+     * a way of obtaining the user's unique ID. The format of the ID is dependent
+     * on the implementation but is generally expected to be a non-null, non-empty
+     * string that uniquely identifies a user within the system.
+     * </p>
+     *
+     * @return A {@code String} representing the unique identifier of the user.
+     */
     public abstract String getUserID();
 }
